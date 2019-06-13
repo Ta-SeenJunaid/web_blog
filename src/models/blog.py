1a8e4f46596f8cd218d188227b0fde0746e066a0
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-06-14 05:30:34 +0600

    Debug blog.py

--- src/models/blog.py
+++ src/models/blog.py
@@ -1,4 +1,4 @@
-author = 'Ta-Seen Junaid'
+__author__ = 'Ta-Seen Junaid'
 
 import uuid
 import datetime
@@ -6,21 +6,21 @@ from src.models.post import Post
 from src.common.database import Database
 
 class Blog(object):
-    def init__(self, author, title, description, author_id, _id=None):
+    def __init__(self, author, title, description, author_id, _id=None):
         self.author = author
         self.author_id = author_id
         self.title = title
         self.description = description
         self._id = uuid.uuid4().hex if _id is None else _id
 
-    def new_post(self, title, content, date = datetime.datetime.utcnow()):
+    def new_post(self, title, content, date=datetime.datetime.utcnow()):
         post = Post(blog_id=self._id,
                     title=title,
                     content=content,
                     author=self.author,
-                    date=date)
-        post.save_to_mongo()
+                    created_date=date)
 
+        post.save_to_mongo()
 
     def get_posts(self):
         return Post.from_blog(self._id)
@@ -39,17 +39,14 @@ class Blog(object):
             '_id': self._id
         }
 
-
     @classmethod
     def from_mongo(cls, id):
         blog_data = Database.find_one(colllection='blogs',
                                       query={'_id': id})
         return cls(**blog_data)
 
-
     @classmethod
     def find_by_author_id(cls, author_id):
         blogs = Database.find(colllection='blogs',query={'author_id':author_id})
 
         return [cls(**blog) for blog in blogs]
-
