41f013fc49d1ea3c5c3c38596250fde4424af3cf
Ta-SeenJunaid <taseen.junaid@gmail.com>
2019-06-18 23:53:29 +0600

    blog_posts module in app.py is added

--- src/app.py
+++ src/app.py
@@ -57,7 +57,12 @@ def user_blogs(user_id=None):
 
     return render_template("user_blogs.html", blogs=blogs, email = user.email)
 
+@app.route('/posts/<string:blog_id>')
+def blog_posts(blog_id):
+    blog = Blog.from_mongo(blog_id)
+    posts = blog.get_posts()
 
+    return render_template('posts.html', posts=posts, blog_title=blog.title)
 
 if __name__=='__main__':
     app.run(port = 4996, debug=True)
